88c06e77d8da94543e2df2218f759585006c5a8a
Johan Droz <johan.droz@gmail.com>
2017-08-27 20:01:52 +0200

    Initial commit for the implementation of the NIST SP800-38G Recommendation for Block Cipher Mode of Operation: Methods for Format-Preserving Encryption

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # fpe
+This is an implementation of the NIST Special Publication 800-38G, Recommendation for Block Cipher Modes of Operation: Methods for Format-Preserving Encryption.
+
+Implementations of the FF1 and FF3 schemes are provided.
+
+FF1 example:
+```golang
+// FF1 Example
+var key = make([]byte, 16)
+rand.Read(key)
+var tweak = make([]byte, 20)
+rand.Read(tweak)
+var radix = uint32(10)
+var plaintext = []uint16{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+// Create AES Block used by FF1.
+var aesBlock, err = aes.NewCipher(key)
+if err != nil {
+	fmt.Printf("NewCipher = %s", err)
+}
+
+// Create CBC mode used by FF1.
+var iv = make([]byte, 16)
+var cbcMode = cipher.NewCBCEncrypter(aesBlock, iv)
+
+// Create FF1 Encrypter and encrypt numeral string
+var encrypter = fpe.NewFF1Encrypter(aesBlock, cbcMode, tweak, radix)
+// We use NumeralStringToBytes and BytesToNumeralString to be compliant with the Go BlockMode interface.
+var x = fpe.NumeralStringToBytes(plaintext)
+encrypter.CryptBlocks(x, x)
+var ciphertext = fpe.BytesToNumeralString(x)
+
+// Create FF1 Decrypter and decrypt numeral string
+var decrypter = fpe.NewFF1Decrypter(aesBlock, cbcMode, tweak, radix)
+// We use NumeralStringToBytes and BytesToNumeralString to be compliant with the Go BlockMode interface.
+var y = fpe.NumeralStringToBytes(ciphertext)
+decrypter.CryptBlocks(y, y)
+var decrypted = fpe.BytesToNumeralString(y)
+
+// Print values
+fmt.Printf("FF1 example:\n")
+fmt.Printf("Plaintext:  %d\n", plaintext)
+fmt.Printf("Ciphertext: %d\n", ciphertext)
+fmt.Printf("Decrypted:  %d\n", decrypted)
+```
+
+FF3 example:
+Note that there is a specificity with the FF3 algorithm. The standard specifies that we must revert the bytes of the symmetric key (see: aes.NewCipher(fpe.RevB(key)). 
+If this is not done, it will affect interoperability.
+```golang
+// FF3 Example
+var key = make([]byte, 16)
+rand.Read(key)
+var tweak = make([]byte, 8)
+rand.Read(tweak)
+var radix = uint32(10)
+var plaintext = []uint16{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+// Create AES Block used by FF3.
+var aesBlock, err = aes.NewCipher(fpe.RevB(key))
+if err != nil {
+	fmt.Printf("NewCipher = %s", err)
+}
+
+// Create FF3 Encrypter and encrypt numeral string
+var encrypter = fpe.NewFF3Encrypter(aesBlock, tweak, radix)
+// We use NumeralStringToBytes and BytesToNumeralString to be compliant with the Go BlockMode interface.
+var x = fpe.NumeralStringToBytes(plaintext)
+encrypter.CryptBlocks(x, x)
+var ciphertext = fpe.BytesToNumeralString(x)
+
+// Create FF3 Decrypter and decrypt numeral string
+var decrypter = fpe.NewFF3Decrypter(aesBlock, tweak, radix)
+// We use NumeralStringToBytes and BytesToNumeralString to be compliant with the Go BlockMode interface.
+var y = fpe.NumeralStringToBytes(ciphertext)
+decrypter.CryptBlocks(y, y)
+var decrypted = fpe.BytesToNumeralString(y)
+
+// Print values
+fmt.Printf("FF3 example:\n")
+fmt.Printf("Plaintext:  %d\n", plaintext)
+fmt.Printf("Ciphertext: %d\n", ciphertext)
+fmt.Printf("Decrypted:  %d\n", decrypted)
+```
+
+To encipher data such as Strings or Credit Cards, see the [helpers](https://github.com/cloudtrust/fpe-field-format).
+
+There are attacks on the NIST standard:
+The first is described in the publication [Message-recovery attacks on Feistel-based Format Preserving Encryption](https://eprint.iacr.org/2016/794.pdf) by Bellare, Hoang, and Tessaro. On page 5 of the same document, the authors suggest a simple fix: increasing the number of Feistel rounds.
+The second attack targets FF3 and is described in the publication [Breaking The FF3 Format-Preserving Encryption Standard Over Small Domains](https://eprint.iacr.org/2017/521.pdf) by F. Durak and S. Vaudenay. The publication also describes how to fix FF3. The NIST published a [statement](https://beta.csrc.nist.gov/News/2017/Recent-Cryptanalysis-of-FF3) and expects to revise the standard, either to fix or withdraw FF3.
+We provide the branch fpe-fix that includes both fixes. That is an augmented number of rounds and the modification of FF3. However, this branch does not comply with the standard anymore, and there are no test vectors to validate it.
+
---
+++ fpe/ff3.go
@@ -0,0 +1,248 @@
+// FF3 (Format-preserving, Feistel-based encryption) mode.
+//
+// Format-preserving encryption (FPE) is designed for data that is not
+// necessarily binary. In particular, given any finite set of symbols,
+// like the decimal numerals, a method for FPE transforms data that is
+// formatted as a sequence of the symbols in such a way that the encrypted
+// form of the data has the same format, including the length, as the
+// original data.
+//
+// See NIST SP 800-38G, pp 16-19.
+package fpe
+
+import (
+	"math"
+	"math/big"
+	"fmt"
+)
+
+const (
+	// The number of Feistel rounds must be 8.
+	roundsFF3 = 8
+	// The tweak must be 8 bytes.
+	tweakLenFF3 = 8
+	// The radix must be in [2..2^16].
+	minRadixFF3 = 2
+	maxRadixFF3 = 1<<16
+	// The minimum length of the numeral string is 2.
+	minInputLenFF3 = 2
+	// The internal cipher's block size (16 bytes for AES).
+	blockSizeFF3 = 16
+)
+
+type ff3 struct {
+	aesBlock  Block
+	tweak     []byte
+	radix     uint32
+}
+
+func newFF3(aesBlock Block, tweak []byte, radix uint32) *ff3 {
+	return &ff3{
+		aesBlock:  aesBlock,
+		tweak:     dup(tweak),
+		radix:     radix,
+	}
+}
+
+type ff3Encrypter ff3
+
+// NewFF3Encrypter returns a BlockMode which encrypts in FF3 mode, using the given
+// Block. The given block must be AES, the length of tweak must be 64 bits, and
+// the radix must be in [2..2^16].
+func NewFF3Encrypter(aesBlock Block, tweak []byte, radix uint32) BlockMode {
+	if len(tweak) != tweakLenFF3 {
+		panic(fmt.Sprintf("NewFF3Encrypter: tweak must be %d bytes.", tweakLenFF3))
+	}
+	if radix < minRadixFF3 || radix > maxRadixFF3 {
+		panic(fmt.Sprintf("NewFF3Encrypter: radix must be in [%d..%d].", minRadixFF3, maxRadixFF3))
+	}
+	if aesBlock.BlockSize() != blockSizeFF3 {
+		panic(fmt.Sprintf("NewFF3Encrypter: block size must be %d bytes.", blockSizeFF3))
+	}
+	return (*ff3Encrypter)(newFF3(aesBlock, tweak, radix))
+}
+
+func (x *ff3Encrypter) CryptBlocks(dst, src []byte) {
+	var radix = x.radix
+	var tweak = x.tweak
+
+	// Convert the src byte string to a numeral string. We use this to be compliant with the Go BlockMode interface.
+	var numeralString = BytesToNumeralString(src)
+	var n = len(numeralString)
+
+	if n < minInputLenFF3 || n > maxLength(radix) {
+		panic("FF3Encrypter/CryptBlocks: input length not supported.")
+	}
+	if math.Pow(float64(radix), float64(n)) < 100 {
+		panic("FF3Encrypter/CryptBlocks: input length too short.")
+	}
+	if len(dst) < len(numeralString) {
+		panic("FF3Encrypter/CryptBlocks: output smaller than input.")
+	}
+	if !isNumeralStringValid(numeralString, radix) {
+		panic("FF3Encrypter/CryptBlocks: numeral string not valid.")
+	}
+
+	var u = uint32(math.Ceil(float64(n) / 2))
+	var v = uint32(n) - u
+	var a = numeralString[:u]
+	var b = numeralString[u:]
+	var tl = tweak[:4]
+	var tr = tweak[4:]
+
+	for i := uint32(0); i < roundsFF3; i++ {
+		var w []byte
+		var m uint32
+		if i % 2 == 0 {
+			m = u
+			w = tr
+		} else {
+			m = v
+			w = tl
+		}
+		var p = getFF3P(w, i, radix, b)
+		var s = getFF3S(p, x.aesBlock)
+		var y = num(s)
+		var c = getFF3CEnc(a, y, radix, m)
+		copy(a, rev(strMRadix(radix, m, c)))
+		a, b = b, a
+	}
+	copy(dst, NumeralStringToBytes(numeralString))
+}
+
+func (x *ff3Encrypter) BlockSize() int {
+	return blockSizeFF3
+}
+
+func (x *ff3Encrypter) SetTweak(tweak []byte) {
+	if len(tweak) != tweakLenFF3 {
+		panic(fmt.Sprintf("FF3Encrypter/SetTweak: tweak must be %d bytes.", tweakLenFF3))
+	}
+	copy(x.tweak, tweak)
+}
+
+type ff3Decrypter ff3
+
+// NewFF3Decrypter returns a FpeMode which decrypts in FF3 mode, using the given
+// Block. The given block must be AES, the radix must be in [2..2^16], the
+// length of tweak must be 64 bits and the tweak must be the same as the tweak
+// used to encrypt the data.
+func NewFF3Decrypter(aesBlock Block, tweak []byte, radix uint32) BlockMode {
+	if len(tweak) != tweakLenFF3 {
+		panic(fmt.Sprintf("NewFF3Decrypter: tweak must be %d bytes.", tweakLenFF3))
+	}
+	if radix < minRadixFF3 || radix > maxRadixFF3 {
+		panic(fmt.Sprintf("NewFF3Decrypter: radix must be in [%d..%d].", minRadixFF3, maxRadixFF3))
+	}
+	if aesBlock.BlockSize() != blockSizeFF3 {
+		panic(fmt.Sprintf("NewFF3Decrypter: block size must be %d bytes.", blockSizeFF3))
+	}
+	return (*ff3Decrypter)(newFF3(aesBlock, tweak, radix))
+}
+
+func (x *ff3Decrypter) CryptBlocks(dst, src []byte) {
+	var radix = x.radix
+	var tweak = x.tweak
+
+	// Convert the src byte string to a numeral string. We use this to be compliant with the Go BlockMode interface.
+	var numeralString = BytesToNumeralString(src)
+	var n = len(numeralString)
+
+	if n < minInputLenFF3 || n > maxLength(radix) {
+		panic("FF3Decrypter/CryptBlocks: input length not supported.")
+	}
+	if math.Pow(float64(radix), float64(n)) < 100 {
+		panic("FF3Decrypter/CryptBlocks: input length too short.")
+	}
+	if len(dst) < len(src) {
+		panic("FF3Decrypter/CryptBlocks: output smaller than input.")
+	}
+	if !isNumeralStringValid(numeralString, radix) {
+		panic("FF3Decrypter/CryptBlocks: numeral string not valid.")
+	}
+
+	var u = uint32(math.Ceil(float64(n) / 2))
+	var v = uint32(n) - u
+	var a = numeralString[:u]
+	var b = numeralString[u:]
+	var tl = tweak[:4]
+	var tr = tweak[4:]
+
+	for i := roundsFF3-1; i >= 0 ; i-- {
+		var w []byte
+		var m uint32
+		if i % 2 == 0 {
+			m = u
+			w = tr
+		} else {
+			m = v
+			w = tl
+		}
+		var p = getFF3P(w, uint32(i), radix, a)
+		var s = getFF3S(p, x.aesBlock)
+		var y = num(s)
+		var c = getFF3CDec(b, y, radix, m)
+		copy(b, rev(strMRadix(radix, m, c)))
+		a, b = b, a
+	}
+	copy(dst, NumeralStringToBytes(numeralString))
+}
+
+func (x *ff3Decrypter) BlockSize() int {
+	return blockSizeFF3
+}
+
+func (x *ff3Decrypter) SetTweak(tweak []byte) {
+	if len(tweak) != tweakLenFF3 {
+		panic(fmt.Sprintf("FF3Decrypter/SetTweak: tweak must be %d bytes.", tweakLenFF3))
+	}
+	copy(x.tweak, tweak)
+}
+
+// maxLength takes an integer radix. It returns the maximum length of the input numeral string
+// computed as maxlen = 2 * floor(log_radix(2^96)).
+func maxLength(radix uint32) int {
+	return 2 * int(math.Floor(math.Log2(math.Pow(2,96)) / math.Log2(float64(radix))))
+}
+
+// getFF3P takes a byte string w, the integers i, radix and a numeral string x. It returns
+// p = w xor [i]4 || [numRadix(rev(x))]12, where [x]y means x represented as a string of s bytes.
+func getFF3P(w []byte, i, radix uint32, x []uint16) ([]byte){
+	var p = make([]byte, blockSizeFF3)
+
+	p[0] = w[0] ^ byte(i >> 24)
+	p[1] = w[1] ^ byte(i >> 16)
+	p[2] = w[2] ^ byte(i >> 8)
+	p[3] = w[3] ^ byte(i)
+	copy(p[4:], getAsBBytes(numRadix(rev(x), radix), 12))
+
+	return p
+}
+
+// getFF3S takes a byte string p and an AES Block. It returns s = revB(aes.Encrypt(revB(p))).
+func getFF3S(p []byte, aesBlock Block) ([]byte) {
+	var s = RevB(p)
+	aesBlock.Encrypt(s, s)
+	s = RevB(s)
+	return s
+}
+
+// getFF3CEnc takes a numeral string x, and the integers y, radix and m. It returns
+// c = (numRadix(rev(x), radix) + y) mod radix^m.
+func getFF3CEnc(x []uint16, y *big.Int, radix, m uint32) (*big.Int) {
+	var c = numRadix(rev(x), radix)
+	c.Add(c, y)
+	var radixM = big.NewInt(0).Exp(big.NewInt(int64(radix)), big.NewInt(int64(m)), nil)
+	c.Mod(c, radixM)
+	return c
+}
+
+// getFF3CDec takes a numeral string x, and the integers y, radix and m. It returns
+// c = (numRadix(rev(x), radix) - y) mod radix^m.
+func getFF3CDec(x []uint16, y *big.Int, radix, m uint32) (*big.Int) {
+	var c = numRadix(rev(x), radix)
+	c.Sub(c, y)
+	var radixM = big.NewInt(0).Exp(big.NewInt(int64(radix)), big.NewInt(int64(m)), nil)
+	c.Mod(c, radixM)
+	return c
+}
